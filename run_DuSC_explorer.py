5251f6e37ccd937d6df7ebbe672e09781a6c03cf
Shervin Nia <niashervin@gmail.com>
2024-01-22 10:21:09 -0800

    Update run_DuSC_explorer.py
    Updated import line to have proper package name.

--- run_DuSC_explorer.py
+++ run_DuSC_explorer.py
@@ -1,7 +1,7 @@
 """A script to run the GUI. Mainly for debugging. The entrypoint script is the preferred way of
 running program. """
 
-from dusc_explorer import main
+from DuSC_explorer import main
 
 if __name__ == '__main__':
     main()
